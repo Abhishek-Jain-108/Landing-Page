f5e1f5fb0aa66e6fa8dd8b9c6589ded0f2f39f3e
Abhishek Jain <167432905+Abhishek-Jain-108@users.noreply.github.com>
2024-07-25 19:57:55 +0530

    Add files via upload

---
+++ Script/pepsi.js
@@ -0,0 +1,10 @@
+function imgslider(anything)
+{
+    document.querySelector('.pepsi').src = anything;
+}
+
+function bcolor(color)
+{
+    let sec = document.querySelector('.sec');
+    sec.style.background = color;
+}
